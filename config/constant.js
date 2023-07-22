a9add1a62857d4c309e65a0be2c34feee9465aae
0xXiaoChen <1255552127@qq.com>
2023-07-22 20:26:02 +0800

    update seo

--- config/constant.js
+++ config/constant.js
@@ -1,7 +1,8 @@
 import { chains } from "./chains"
 
-export const SITE_NAME = "y=e^x Lab"
-export const SITE_DESCRIPTION = "Next.js + Ethereum starter kit"
+export const SITE_NAME = "y=e^x Dex"
+export const SITE_DESCRIPTION =
+  "A research based Core Contracts development team. Working for innovation in Web 3.0"
 export const SITE_URL = "https://yexlabs.vercel.app"
 
 export const SOCIAL_TWITTER = "Yex_Labs"
