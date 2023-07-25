cc61bc8580dab09f4485f174bfeefbe6a92906c9
0xXiaoChen <1255552127@qq.com>
2023-07-25 20:13:57 +0800

    add swap page

--- pages/swap.jsx
+++ pages/swap.jsx
@@ -1,10 +1,15 @@
+import SwapCard from "@/components/swap/SwapCard"
 import useCheckNetwork from "@/hooks/useCheckNetwork"
 import React from "react"
 
 const swap = () => {
   const { error } = useCheckNetwork()
   // console.log("checkNetworkError:" + error)
-  return <div>swap</div>
+  return (
+    <div>
+      <SwapCard />
+    </div>
+  )
 }
 
 export default swap
