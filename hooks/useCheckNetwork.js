46730061c789d68b08cd39d07aacb84b50be4628
0xXiaoChen <1255552127@qq.com>
2023-11-12 02:59:48 +0800

    update checknetwork and address

--- hooks/useCheckNetwork.js
+++ hooks/useCheckNetwork.js
@@ -1,10 +1,15 @@
 import React, { useEffect } from "react"
 import { useNetwork, useSwitchNetwork } from "wagmi"
+import { useRouter } from "next/router"
 import { ETH_CHAINS } from "@/config/constant"
 
 export default function useCheckNetwork() {
+  const router = useRouter()
+  const pathsUsingSecondChain = ["/launching", "/ilo", "/portfolio"]
   // check if the current chain is the same as the first chain in the ETH_CHAINS array
-  const currentChain = ETH_CHAINS[0]
+  const currentChain = pathsUsingSecondChain.includes(router.pathname)
+    ? ETH_CHAINS[1]
+    : ETH_CHAINS[0]
 
   const { chain } = useNetwork()
   const { chains, error, isLoading, pendingChainId, switchNetwork } =
