2e4adbd98503db2f2142e194a99dfe787e8170f9
zongzheng <304389312@qq.com>
2024-02-03 17:38:53 +0800

    fix image styles
    Signed-off-by: zongzheng <304389312@qq.com>

--- dist/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/zongzheng/Projects/Yex-Labs/app/layout\":[\"static/media/86fdec36ddd9097e-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- dist/static/development/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/launchtoken":["static\u002Fchunks\u002Fpages\u002Flaunchtoken.js"],sortedPages:["\u002F_app","\u002F_error","\u002Flaunchtoken"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
